f18e8c33f5ff7d3a59da1cfa78cbd4f39fdb8a50
Vikash Kumar <vikash.mvk@gmail.com>
2025-06-10 19:37:15 +0530

    Refactor: Move functions to 'Functions' directory
    This commit moves several Python files containing functions into a new 'Functions' directory to improve project organization.
    
    The following files were moved:
    - Album_dict_function.py
    - Album_dict_function_loop.py
    - Function_Message.py
    - Imports.py
    - Sandwiches_func.py
    - T_shirt_message.py
    - car_make_func.py
    - check_module.py
    - city_country().py
    - describe_city.py
    - favorite_book.py
    - messages_func.py
    - send_messages().py
    
    Additionally, 'Test.py' was modified to reflect the new file paths, and two new files, 'class Restaurant.py' and 'my_calculator.py', were added.

---
+++ class Restaurant.py
@@ -0,0 +1,4 @@
+class Restaurant (self,res_name,cusine):
+    def __init__():
+        self.res_name = res_name
+        self.cusine = cusine
---
+++ my_calculator.py
@@ -0,0 +1,74 @@
+'''Simple Calculator'''
+
+print('''
+     _____ _                 __                     
+  / ___/(_)___ ___  ____  / /__                   
+  \__ \/ / __ `__ \/ __ \/ / _ \                  
+ ___/ / / / / / / / /_/ / /  __/                  
+/____/_/_/ /_/ /_/ .___/_/\___/                   
+   ______      _/_/         __      __            
+  / ____/___ _/ /______  __/ /___ _/ /_____  _____
+ / /   / __ `/ / ___/ / / / / __ `/ __/ __ \/ ___/
+/ /___/ /_/ / / /__/ /_/ / / /_/ / /_/ /_/ / /    
+\____/\__,_/_/\___/\__,_/_/\__,_/\__/\____/_/     ''')
+
+def addition(a,b):
+    return f'Result of this addition is {a+b}'
+
+def subtraction(a,b):
+    return f'Result of this subtraction is {a-b}'
+
+def multiplication(a,b):
+    return f'Result of this multiplication is {a*b}'
+
+def division(a,b):
+    if b==0 :
+        return  '\n***Cannot divide by zero***\n'
+        
+    return f'Result of this division is {a/b}'
+
+
+while True:
+    print("="*90)
+
+
+    print("Choose the type of calculation you want to perform:")
+
+    print  ('''
+  Enter 1 for Addition
+        2 for Subtraction
+        3 for Multiplication
+        4 for Division
+        5 for Exit
+              ''')
+    calculation_type=int(input('Enter your choice: '))
+    if calculation_type == 5:
+        break
+    elif calculation_type == 1:      
+        print(f'first number + second number = ')              
+        a=int(input('Enter first number: '))
+        b=int(input('Enter second number: '))
+        print(addition(a,b))
+        input('press enter to continue...')
+    elif calculation_type== 2:
+        print(f'first number - second number = ')
+        a=int(input('Enter first number: '))
+        b=int(input('Enter second number: '))
+        print(subtraction(a,b))
+        input('press enter to continue...')
+    elif calculation_type== 3:
+        print(f'first number * second number = ')
+        a=int(input('Enter first number: '))
+        b=int(input('Enter second number: '))
+        print(multiplication(a,b))
+        input('press enter to continue...')
+    elif calculation_type== 4:
+        print(f'first number / second number = ')
+        a=int(input('Enter first number: '))
+        b=int(input('Enter second number: '))
+        print(division(a,b))
+        input('press enter to continue...')
+    else:
+        print('Please provide a valid input')
+        input('press enter to continue...')
+        
